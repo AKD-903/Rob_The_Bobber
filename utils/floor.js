9f99f7ce3ea3d8876d1a9dad6ad304340e46e66a
AKD-903 <deyarjun99@gmail.com>
2026-04-06 15:10:37 -0400

    Update floor.js

--- utils/floor.js
+++ utils/floor.js
@@ -1,14 +1,14 @@
 class Floor {
   constructor(y_pos) {
       this.y = y_pos;
-      this.fillColor = "#000000"
+      this.fillColor = "#4B2E19"
       this.width = 1000;
       this.height = 30;
 
   }
 
   show() {
-      fill("#FF0000");
+      fill("#4B2E19");
       rect(0, this.y, this.width, this.height);
   }
 }
